fe7dbe242befb9d978cf1333d689bf16b7f6cb48
albertoleoncio <40328018+albertoleoncio@users.noreply.github.com>
2020-06-05 18:55:12 -0300

    Create urc.php

---
+++ urc.php
@@ -0,0 +1,51 @@
+<?php
+include 'globals.php';
+echo "<pre>";
+
+//Login
+$wiki = new Wikimate($api_url);
+if ($wiki->login('AlbeROBOT', $password))
+	echo 'Login OK<hr>' ;
+else {
+	$error = $wiki->getError();
+	echo "<b>Wikimate error</b>: ".$error['login'];
+	die();
+}
+
+if (isset($_POST["nome"])) {
+
+	$lista = explode("\n", $_POST["nome"]);
+
+	foreach ($lista as $pagina) {
+		echo $pagina."\n";
+		$file = @$wiki->getFile(trim($pagina));
+		if (!$file->exists()) {
+			echo "File not found\n";
+			break;
+		}
+		$history = $file->getHistory(true);
+
+		//print_r($history);
+		//echo "<hr>";
+
+		if ($file->getHeight() > 500) {
+			echo "Height > 500\n";
+			break;
+		}
+
+		for ($i=1; $i < count($history); $i++) { 
+			$archivename = $file->getArchivename($i);
+			//$file->delete('bot: Solicitado em [[Special:diff/58435108|58435108]]', $archivename);
+			echo $archivename." processado\n";
+		}
+		echo "<hr>";
+	}
+}
+?>
+
+Separar arquivo por cada linha
+<br>
+<form action="/alberobot/test.php" method="post">
+  <textarea rows="6" cols="50" name="nome"></textarea>
+  <input type="submit">
+</form>
