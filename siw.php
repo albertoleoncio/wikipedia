95a0381723dccb20bd1f4c3cbfa5067c35cb48e5
A-Infor <67929816+A-Infor@users.noreply.github.com>
2020-07-06 19:10:27 -0300

    Desambiguação de nomes de variáveis (#4)
    * Desambiguação da variável "nome"
    
    É "nome" do artigo ou do usuário? Agora se chama "artigo_titulo", pra evitar confusão.
    
    * Desambiguação da variável "artigo"
    
    A variável "artigo" é o conteúdo do artigo? O histórico do artigo? Não, é a lista de editores do artigo. Estou deixando isso claro.

--- siw.php
+++ siw.php
@@ -2,16 +2,16 @@
 echo "<pre>";
 
 //Verifica se alguma página foi informada
-if ($_GET["nome"]) {
+if ($_GET["artigo_titulo"]) {
 
 	//Introdução da ferramenta
-	echo "Editores do artigo <b>".trim($_GET["nome"])."</b>:<br><small>Inativo: +90 dias</small><br><br>";
+	echo "Editores do artigo <b>".trim($_GET["artigo_titulo"])."</b>:<br><small>Inativo: +90 dias</small><br><br>";
 
 	//Coleta nome dos usuários não-bot que editaram o artigo
-	$artigo = pos(json_decode(file_get_contents("https://pt.wikipedia.org/w/api.php?action=query&format=json&prop=contributors&titles=".urlencode(trim($_GET["nome"]))."&pcexcluderights=bot&pclimit=max"), true)['query']['pages'])['contributors'];
+	$editores_artigo = pos(json_decode(file_get_contents("https://pt.wikipedia.org/w/api.php?action=query&format=json&prop=contributors&titles=".urlencode(trim($_GET["artigo_titulo"]))."&pcexcluderights=bot&pclimit=max"), true)['query']['pages'])['contributors'];
 
 	//Loop de verificação dos usuários
-	foreach ($artigo as $list) {
+	foreach ($editores_artigo as $list) {
 
 		//Coleta informações do usuário
 		$user = json_decode(file_get_contents("https://pt.wikipedia.org/w/api.php?action=query&format=json&list=users%7Cusercontribs&usprop=blockinfo&uclimit=1&ususers=".urlencode($list['name'])."&ucuser=".urlencode($list['name'])), true)['query'];
@@ -30,13 +30,13 @@ if ($_GET["nome"]) {
 		}
 
 		//Retorna link para envio de aviso
-		echo "<a target='_blank' href='https://pt.wikipedia.org/w/index.php?title=User_talk:".urlencode($list['name'])."&action=edit&section=new&preload=Predefini%C3%A7%C3%A3o:Aviso-ESR-SIW/Preload&preloadparams%5b%5d=".urlencode(trim($_GET["nome"]))."&preloadparams%5b%5d='>".$list['name']."</a><br>";
+		echo "<a target='_blank' href='https://pt.wikipedia.org/w/index.php?title=User_talk:".urlencode($list['name'])."&action=edit&section=new&preload=Predefini%C3%A7%C3%A3o:Aviso-ESR-SIW/Preload&preloadparams%5b%5d=".urlencode(trim($_GET["artigo_titulo"]))."&preloadparams%5b%5d='>".$list['name']."</a><br>";
 	}
 }
 
 //Formulário de submissão do nome do artigo
 ?><br>
 <form action="/alberobot/siw.php" method="get">
-  <input type="text" placeholder="Nome do artigo" name="nome">
+  <input type="text" placeholder="Nome do artigo" name="artigo_titulo">
   <input type="submit">
 </form>
