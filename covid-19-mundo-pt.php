dc7afe126153f289196700f6a15007a14e5801d6
albertoleoncio <40328018+albertoleoncio@users.noreply.github.com>
2020-08-06 23:57:17 -0300

    pt.wiki: bug fix

--- covid-19-mundo-pt.php
+++ covid-19-mundo-pt.php
@@ -15,7 +15,10 @@ function refparser($ref) {
 		" April 2020",
 		" May 2020",
 		" June 2020",
-		" July 2020"
+		" July 2020",
+		" August 2020",
+		" September 2020",
+		" Octuber 2020"
 	);
 	$para = array(
 		"",
@@ -23,7 +26,10 @@ function refparser($ref) {
 		"-04-2020",
 		"-05-2020",
 		"-06-2020",
-		"-07-2020"
+		"-07-2020",
+		"-08-2020",
+		"-09-2020",
+		"-10-2020"
 	); 
 	$refparsed = str_replace($de, $para, preg_replace('/date=([0-9]{4})-([0-9]{2})-([0-9]{2})/', 'date=$3-$2-$1', trim($ref)));
 	return $refparsed;
