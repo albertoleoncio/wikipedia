f3c08b7a4cc581059bf67a2a1076338305e1d992
albertoleoncio <40328018+albertoleoncio@users.noreply.github.com>
2020-08-22 13:32:41 -0300

    covid-19-brasil.php: Limpeza, simplificação, reorganização e adição de funcão de atualização de gráfico

--- covid-19-brasil.php
+++ covid-19-brasil.php
@@ -1,6 +1,12 @@
 <?php
 include './bin/globals.php';
 
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Coleta de dados do site do Ministério da Saúde
+//
+////////////////////////////////////////////////////////////////////////////////////////
+
 //Recupera dados da fonte - Adaptado de https://github.com/wcota/covid19br/blob/master/scrape-covid-saude-gov-br.sh
 $headers = array(
     'X-Parse-Application-Id: unAFkcaNDeXajurGB7LChj8SgQYS2ptm',
@@ -19,27 +25,26 @@ $PortalSintese = json_decode($result, true);
 $confirmado = str_replace(".", "", $PortalSintese[0]['casosAcumuladoN']);
 $obitos = str_replace(".", "", $PortalSintese[0]['obitosAcumuladoN']);
 $recuperados = str_replace(".", "", $PortalSintese[0]['Recuperadosnovos']);
-$datahora = explode("T", $PortalSintese[0]['updated_at']);
-$horacompleta = explode(":", $datahora[1]);
-$horacompleta[0] = $horacompleta[0]-3;
-$hora = $horacompleta[0].":".$horacompleta[1];
-$datacompleta = explode("-", $datahora[0]);
-$dia = $datacompleta[2];
-$mes = $datacompleta[1];
-$ano = $datacompleta[0];
+$timestamp = strtotime($PortalSintese[0]['updated_at']);
 
 //Proteção contra erro
 if (!isset($confirmado) OR $confirmado == FALSE) die("Erro no total de confirmados");
 
 //Construção do wikitexto dos dados gerais
-$saida = "\n"."-->{{#ifeq:{{{1}}}|confirmados|".$confirmado."|}}<!--\n".
+$saida =  "\n-->{{#ifeq:{{{1}}}|confirmados|".$confirmado."|}}<!--\n".
 			"-->{{#ifeq:{{{1}}}|obitos|".$obitos."|}}<!--\n".
 			"-->{{#ifeq:{{{1}}}|recuperados|".$recuperados."|}}<!--\n".
-			"-->{{#ifeq:{{{1}}}|data|{{DataExt|".$dia."|".$mes."|".$ano."}}|}}<!--\n".
-			"-->{{#ifeq:{{{1}}}|hora|".$hora."|}}<!--\n";
+			"-->{{#ifeq:{{{1}}}|data|{{DataExt|".date("d|m|Y",$timestamp)."}}|}}<!--\n".
+			"-->{{#ifeq:{{{1}}}|hora|".date("H:i",$timestamp)."|}}<!--\n";
 
 //Merge das arrays das regiões brasileiras
-$SinteseUFs = array_merge($PortalSintese[1]['listaMunicipios'], $PortalSintese[2]['listaMunicipios'], $PortalSintese[3]['listaMunicipios'], $PortalSintese[4]['listaMunicipios'], $PortalSintese[5]['listaMunicipios']);
+$SinteseUFs = array_merge(
+	$PortalSintese[1]['listaMunicipios'], 
+	$PortalSintese[2]['listaMunicipios'], 
+	$PortalSintese[3]['listaMunicipios'], 
+	$PortalSintese[4]['listaMunicipios'], 
+	$PortalSintese[5]['listaMunicipios']
+);
 
 //Ordenar unidades federativas por ordem alfabética
 usort($SinteseUFs, function ($a, $b) {
@@ -56,11 +61,11 @@ foreach ($SinteseUFs as $linha) {
 	$x++;
 }
 
-//Loop para construção do wikitexto das UFs
-for ($y = 0; $y < $x; $y++) {
-    $linha = "-->{{#ifeq:{{{1}}}|".$UFs[1][$y]."-c|".$UFs[2][$y]."|}}<!--\n-->{{#ifeq:{{{1}}}|".$UFs[1][$y]."-o|".$UFs[3][$y]."|}}<!--\n";
-    $saida = $saida.$linha;
-}
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Atualiza dados da predefinição
+//
+////////////////////////////////////////////////////////////////////////////////////////
 
 //Login
 $wiki = new Wikimate($api_url);
@@ -76,6 +81,12 @@ $page = $wiki->getPage('Predefinição:Números de casos de COVID-19 por Unidade
 if (!$page->exists()) die('Page not found');
 $wikiCode = $page->getText();
 
+//Loop para construção do wikitexto das UFs
+for ($y = 0; $y < $x; $y++) {
+    $linha = "-->{{#ifeq:{{{1}}}|".$UFs[1][$y]."-c|".$UFs[2][$y]."|}}<!--\n-->{{#ifeq:{{{1}}}|".$UFs[1][$y]."-o|".$UFs[3][$y]."|}}<!--\n";
+    $saida = $saida.$linha;
+}
+
 //Substituição do código antigo da predefinição pelo código novo
 $pieces = explode("%", $wikiCode);
 $pieces[1] = $saida;
@@ -83,9 +94,54 @@ $wikiCode = implode("%", $pieces);
 
 //Gravar código
 if ($page->setText($wikiCode, 0, true, "bot: Atualizando estatísticas")) {
-	echo "\nEdição realizada.\n";
+	echo "\nEdição em predefinição realizada.\n";
 } else {
 	$error = $page->getError();
+	echo "\nError: " . print_r($error, true) . "\n";
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Atualiza dados do Gráfico
+//
+////////////////////////////////////////////////////////////////////////////////////////
+
+//Formata string de dia compatível com predefinição 
+$dia_atualização = date("d-m-Y",$timestamp);
+
+//Formata string de nova linha para atualização
+$nova_linha = $dia_atualização.";".$obitos.";".$recuperados.";".$confirmado."";
+
+//Recupera dados da predefinição
+$page2 = $wiki->getPage('Predefinição:Dados da pandemia de COVID-19/Gráfico de casos médicos no Brasil');
+if (!$page2->exists()) die('Page not found');
+
+//Converte página em array, separando por linhas
+$código = explode("\n", $page2->getText());
+
+//Procura linha que consta após lista de dias na predefinição
+$key = array_search("|caption='''Fontes:'''", $código);
+
+//Identifica última atualização da predefinição
+$ultima_atualização = substr($código[$key-1], 0, 10);
+
+//Verifica se o dia da atualização corresponde a última atualização na predefinição
+//Caso não corresponda, insere a nova linha. Caso corresponda, verifica a última atualização
+//Caso seja diferente, corrige-a com os dados atualizados
+if ($dia_atualização != $ultima_atualização) {
+	array_splice($código, $key, 0, $nova_linha);
+} else {
+	if ($código[$key-1] != $nova_linha) $código[$key-1] = $nova_linha;
+}
+
+//Remonta código
+$código = implode("\n", $código);
+
+//Gravar código
+if ($page2->setText($código, 0, true, "bot: Atualizando estatísticas")) {
+	echo "\nGráfico atualizado.\n";
+} else {
+	$error = $page2->getError();
 	echo "\nError: " . print_r($error, true) . "\n";
 }
 
