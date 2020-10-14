1cfa91abbb845024d9e33d2cbd14277dad92a704
albertoleoncio <40328018+albertoleoncio@users.noreply.github.com>
2020-10-14 09:24:08 -0300

    ead.php: bug fix

--- ead.php
+++ ead.php
@@ -2,11 +2,11 @@
 
 //Recupera artigo atual
 $atual = json_decode(file_get_contents("https://pt.wikipedia.org/w/api.php?action=expandtemplates&format=json&text=%7B%7BEm%20destaque%2Flistagem%7C%7B%7BEm%20destaque%2Fcontador%7D%7D%7D%7D&prop=wikitext"), true)['expandtemplates']['wikitext'];
-if ($atual === FALSE) die("Nao foi possível recuperar os dados.");
+if ($atual === FALSE OR $atual == "") die("Nao foi possível recuperar os dados.");
 
 //Recupera último artigo publicado
 $ultimo = file_get_contents("https://pt.wikipedia.org/w/index.php?title=Usu%C3%A1rio(a):AlbeROBOT/EAD&action=raw");
-if ($ultimo === FALSE) die("Nao foi possível recuperar os dados.");
+if ($ultimo === FALSE OR $ultimo == "") die("Nao foi possível recuperar os dados.");
 
 //Encerra script caso o último artigo publicado seja o artigo atual
 if ($atual == $ultimo) die("Nada a alterar!");
