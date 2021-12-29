8d6fe8e7b70d95907894d49984874af5049ead0d
albertoleoncio <40328018+albertoleoncio@users.noreply.github.com>
2021-12-29 00:46:32 -0300

    api.php: +uploadAPI

--- bin/api.php
+++ bin/api.php
@@ -197,3 +197,87 @@ function getsectionsAPI( $page ) {
 	return $allsections;
 
 }
+
+
+function uploadAPI ( $text, $location, $summary, $page, $userAPI) {
+	global $endPoint;
+
+	//Get token
+	$params1 = [
+		"action" 	=> "query",
+		"meta" 		=> "tokens",
+		"format" 	=> "json"
+	];
+
+	$url1 = $endPoint . "?" . http_build_query( $params1 );
+
+	$ch1 = curl_init( $url1 );
+
+	curl_setopt( $ch1, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt( $ch1, CURLOPT_COOKIEJAR, $userAPI."_cookie.inc" );
+	curl_setopt( $ch1, CURLOPT_COOKIEFILE, $userAPI."_cookie.inc" );
+
+	$output1 = curl_exec( $ch1 );
+	curl_close( $ch1 );
+
+	$result1 = json_decode( $output1, true );
+	$csrftoken = $result1["query"]["tokens"]["csrftoken"];
+
+	//Get siteinfo
+	$params2 = [
+		"action" 	=> "query",
+		"meta" 		=> "siteinfo",
+		"siprop" 	=> "general|fileextensions",
+		"format" 	=> "json"
+	];
+
+	$url2 = $endPoint . "?" . http_build_query( $params2 );
+
+	$ch2 = curl_init( $url2 );
+
+	curl_setopt( $ch2, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt( $ch2, CURLOPT_COOKIEJAR, $userAPI."_cookie.inc" );
+	curl_setopt( $ch2, CURLOPT_COOKIEFILE, $userAPI."_cookie.inc" );
+
+	$output2 = curl_exec( $ch2 );
+	curl_close( $ch2 );
+
+	$result2 = json_decode( $output2, true );
+	$max_upload_size = $result2["query"]["general"]["maxuploadsize"];
+	foreach ($result2["query"]["fileextensions"] as $ext) $allowed_extensions[] = $ext["ext"];
+
+	//Verifications about the file
+	if (!file_exists($location)) return FALSE;
+	if (!in_array(pathinfo($location)['extension'], $allowed_extensions)) return FALSE;
+	if (filesize($location) > $max_upload_size) return FALSE;
+
+
+	$params4 = [
+		"action" 		=> "upload",
+		"filename" 		=> $page,
+		"comment" 		=> $summary,
+		"text"			=> $text,
+		"file"			=> curl_file_create($location, mime_content_type($location), $page),
+		"token" 		=> $csrftoken,
+		"ignorewarnings"=> "1",
+		"format" 		=> "json"
+	];
+
+	$ch = curl_init();
+
+	curl_setopt( $ch, CURLOPT_URL, $endPoint );
+	curl_setopt( $ch, CURLOPT_POST, true );
+	curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: multipart/form-data'));
+	curl_setopt( $ch, CURLOPT_POSTFIELDS, $params4 );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt( $ch, CURLOPT_COOKIEJAR, $userAPI."_cookie.inc" );
+	curl_setopt( $ch, CURLOPT_COOKIEFILE, $userAPI."_cookie.inc" );
+
+	$output = curl_exec( $ch );
+	curl_close( $ch );
+
+	echo ( "<pre style=\"background-color: antiquewhite;\">" );
+	$output = json_decode( $output, true );
+	print_r ( $output );
+	echo ( "</pre>" );
+}
