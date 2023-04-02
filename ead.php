de93449dc4c89979f5b87c0fea8142930954816c
albertoleoncio <40328018+albertoleoncio@users.noreply.github.com>
2023-04-02 18:53:51 -0300

    ead.php: Convertendo para OO

--- ead.php
+++ ead.php
@@ -1,39 +1,100 @@
 <pre><?php
-
-//Recupera artigo atual
-$atual = json_decode(file_get_contents("https://pt.wikipedia.org/w/api.php?action=expandtemplates&format=json&text=%7B%7BEm%20destaque%2Flistagem%7C%7B%7BEm%20destaque%2Fcontador%7D%7D%7D%7D&prop=wikitext"), true)['expandtemplates']['wikitext'];
-if ($atual === FALSE OR $atual == "") die("Nao foi possível recuperar os dados.");
-
-//Recupera último artigo publicado
-$ultimo = file_get_contents("https://pt.wikipedia.org/w/index.php?title=Usu%C3%A1rio(a):AlbeROBOT/EAD&action=raw");
-if ($ultimo === FALSE OR $ultimo == "") die("Nao foi possível recuperar os dados.");
-
-//Encerra script caso o último artigo publicado seja o artigo atual
-if ($atual == $ultimo) die("Nada a alterar!");
-
-//Login
 require_once './bin/globals.php';
-require_once './bin/api.php';
-loginAPI($username, $password);
-
-//Define página de usuário
-$page = 'Usuário(a):AlbeROBOT/EAD';
-
-//Gravar código
-editAPI($atual, 0, true, "bot: Atualizando EAD", $page, $username);
-
-//Monta status para envio ao Twitter
-$twitter_status = $atual." é um artigo de destaque na Wikipédia!\n\nIsso significa que ele foi identificado como um dos melhores artigos produzidos pela comunidade da Wikipédia.\n\nO que achou? Ainda tem como melhorar?\nhttps://pt.wikipedia.org/wiki/".rawurlencode($atual);
-
-//Envia Tweet
-require_once "tpar/twitteroauth/autoload.php";
+require_once './bin/api2.php';
+require_once './tpar/twitteroauth/autoload.php';
 use Abraham\TwitterOAuth\TwitterOAuth;
-define('CONSUMER_KEY', $twitter_consumer_key);
-define('CONSUMER_SECRET', $twitter_consumer_secret);
-$twitter_conn = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $twitter_access_token, $twitter_access_token_secret);
-$post_tweets = $twitter_conn->post("statuses/update", ["status" => $twitter_status]);
 
-//Retorna resultado
-print_r($post_tweets->created_at);
-print_r($post_tweets->id);
-echo("\nOK!");
+class Ead extends WikiAphpi {
+
+    /**
+    * Get the current featured article on the Portuguese Wikipedia.
+    * @throws Exception if the API response does not contain the expected data
+    * @return string the title of the current featured article
+    */
+    private function getCurrentArticle() {
+        $queryParams = [
+            'action' => 'expandtemplates',
+            'format' => 'php',
+            'text' => '{{Em destaque/listagem|{{Em destaque/contador}}}}',
+            'prop' => 'wikitext'
+        ];
+        $response = $this->see($queryParams);
+        $currentArticle = $response['expandtemplates']['wikitext'] ?? false;
+        if (!$currentArticle) {
+            throw new Exception(print_r($response, true));
+        }
+        return $currentArticle;
+    }
+
+    /**
+     * Retrieves the content of a log page containing the title of the last article published.
+     * @param string $logPage The title of the log page to retrieve.
+     * @return string The content of the log page.
+     */
+    private function getLastPublishedArticle($logPage) {
+        return $this->get($logPage);
+    }
+
+    /**
+     * Posts a tweet announcing that the given article is a featured article on Wikipedia.
+     * @param string $currentArticle The title of the featured article.
+     * @param array $tokens An array containing the Twitter API consumer key, consumer secret, access token, and access token secret.
+     * @return int The ID of the posted tweet on success.
+     */
+    private function doTweet($currentArticle, $tokens) {
+        $twitter_status  = "{$currentArticle} é um artigo de destaque na Wikipédia!\n\n";
+        $twitter_status .= "Isso significa que ele foi identificado como um dos melhores artigos produzidos pela comunidade da Wikipédia.\n\n";
+        $twitter_status .= "O que achou? Ainda tem como melhorar?\n";
+        $twitter_status .= "https://pt.wikipedia.org/wiki/".rawurlencode($currentArticle);
+        $twitter_conn = new TwitterOAuth(...$tokens);
+        $post_tweets = $twitter_conn->post("statuses/update", ["status" => $twitter_status]);
+        return $post_tweets->id;
+    }
+
+    /**
+     * Update the log page with the current featured article.
+     * @param string $currentArticle The title of the current featured article.
+     * @param string $logPage The title of the log page.
+     * @return bool Returns true on success or false on failure.
+     */
+    private function doLog($currentArticle, $logPage) {
+        return $this->edit($currentArticle, 0, true, "bot: Atualizando EAD", $logPage);
+    }
+
+    /**
+     * Runs the script by retrieving the current featured article from Wikipedia,
+     * checking whether it is different from the last published one by reading
+     * a log page, and publishing a tweet and updating the log page if necessary.
+     * @param array $tokens An array containing the Twitter API access token, access token secret,
+     *                      consumer key, and consumer secret, in this order.
+     * @return array An array containing the ID of the tweet posted and the title of the featured article 
+     *               (or the log page if the article has not changed since the last run).
+     */
+    public function run($tokens) {
+
+
+        $logPage = 'Usuário(a):AlbeROBOT/EAD';
+        $lastArticle = $this->getLastPublishedArticle($logPage);
+        $currentArticle = $this->getCurrentArticle();
+
+        if ($currentArticle == $lastArticle) {
+            return [$currentArticle, $lastArticle];
+        } else {
+            return [
+                $this->doLog($currentArticle, $logPage),
+                $this->doTweet($currentArticle, $tokens)
+            ];
+        }
+
+    }
+}
+
+//Run script
+$tokens = [
+    $twitter_consumer_key, 
+    $twitter_consumer_secret, 
+    $twitter_access_token, 
+    $twitter_access_token_secret
+];
+$api = new Ead('https://pt.wikipedia.org/w/api.php', $username, $password);
+print_r($api->run($tokens));
