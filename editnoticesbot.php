f07306e40a23d031593e87110f559ece28d1ad2b
albertoleoncio <40328018+albertoleoncio@users.noreply.github.com>
2023-04-06 02:18:46 -0300

    editnotices.php: Convertendo para OO

--- editnoticesbot.php
+++ editnoticesbot.php
@@ -1,140 +1,217 @@
-<?php
+<pre><?php
 require_once './bin/globals.php';
+require_once 'WikiAphpi/main.php';
 
-//Login
-require_once './bin/api.php';
-loginAPI($usernameEN, $passwordEN);
-
-//Funções
-function api_get($params)
+class EditNotices extends WikiAphpiLogged
 {
-	global $usernameEN;
-	$ch1 = curl_init( "https://pt.wikipedia.org/w/api.php?" . http_build_query( $params ) );
-	curl_setopt( $ch1, CURLOPT_RETURNTRANSFER, true );
-	curl_setopt( $ch1, CURLOPT_COOKIEJAR, $usernameEN."_cookie.inc" );
-	curl_setopt( $ch1, CURLOPT_COOKIEFILE, $usernameEN."_cookie.inc" );
-	$data = curl_exec( $ch1 );
-	curl_close( $ch1 );
-	return $data;
+
+    /**
+     * Extract the page title from a MediaWiki page title string.
+     *
+     * @param string $pageTitle The full page title string.
+     * @return string The extracted page title.
+     */
+    private function extractPageTitle($pageTitle)
+    {
+        $pageTitle = str_replace(['MediaWiki:Editnotice-0-', 'Discussão:'], '', $pageTitle);
+        $pageTitle = str_replace('/', '-', $pageTitle);
+        
+        return $pageTitle;
+    }
+
+    /**
+     * Get a list of page titles for a given page key.
+     *
+     * @param array $params An array of API parameters to use in the request.
+     * @param string $pageKey The key of the page list in the API response.
+     * @return array The list of page titles.
+     */
+    private function getPageTitles($params, $pageKey)
+    {
+        $pageTitleList = [];
+
+        do {
+            $api = $this->see($params);
+            foreach ($api["query"][$pageKey] as $page) {
+                $pageTitleList[] = $this->extractPageTitle($page['title']);
+            }
+            $continueKey = key_exists("continue", $api) ? key($api["continue"]) : false;
+            $params[$continueKey] = $api['continue'][$continueKey] ?? false;
+        } while ($params[$continueKey]);
+
+        return $pageTitleList;
+    }
+
+    /**
+     * Get a list of members of a category with the given title.
+     *
+     * @param string $title The title of the category to get the members of.
+     * @return array The list of category members' page titles.
+     */
+    private function getCategoryMembers($title)
+    {
+        $params = [
+            "action"        => "query",
+            "format"        => "php",
+            "list"          => "categorymembers",
+            "cmtitle"       => $title,
+            "cmprop"        => "title",
+            "cmnamespace"   => "1",
+            "cmlimit"       => "max"
+        ];
+        return $this->getPageTitles($params, "categorymembers");
+    }
+
+    /**
+     * Get a list of pages that embed the given page.
+     *
+     * @param string $title The title of the page to get the embedders of.
+     * @return array The list of pages that embed the given page.
+     */
+    private function getEmbeddedIn($title)
+    {
+        $params = [
+            "action"        => "query",
+            "format"        => "php",
+            "list"          => "embeddedin",
+            "eititle"       => $title,
+            "einamespace"   => "8",
+            "eilimit"       => "max"
+        ];
+
+        $list = $this->getPageTitles($params, "embeddedin");
+
+        //The given page is automatically included in the returned 
+        //list to avoid a false positive in the results.
+        $list[] = $this->extractPageTitle($title);
+
+        return $list;
+    }
+
+    /**
+     * Get a list of page titles that should exist based on the given edit notice data.
+     *
+     * @param array $editNoticeData The edit notice data to use to determine which pages should exist.
+     * @return array The list of page titles that should exist.
+     */
+    private function getPagesThatShouldExist($editNoticeData)
+    {
+        $needs = array_merge(
+            $this->getCategoryMembers($editNoticeData["afinidade"]),
+            $this->getCategoryMembers($editNoticeData["escrito"])
+        );
+        return array_unique($needs);
+    }
+
+    /**
+     * Get a list of page titles that already exist based on the given edit notice data.
+     * Note: The page specified by the "afluir" key in the edit notice data is automatically
+     * included in the returned list to avoid a false negative in later methods.
+     *
+     * @param array $editNoticeData The edit notice data to use to determine which pages already exist.
+     * @return array The list of page titles that already exist.
+     */
+    private function getPagesThatAlreadyExist($editNoticeData)
+    {
+        return $this->getEmbeddedIn($editNoticeData["afluir"]);
+    }
+
+    /**
+     * Delete the pages with the specified titles.
+     * Note: The bot will sleep for 10 seconds after  
+     * each page deletion in accordance with the local bot policy.
+     *
+     * @param array $pagesToDelete The list of page titles to delete.
+     */
+    private function deletePages($pagesToDelete)
+    {
+        foreach ($pagesToDelete as $page) {
+            $this->delete(
+                $page,
+                "G1 - [[WP:ER#ERg1|Eliminação técnica]] (bot: Eliminando editnotice desnecessário)"
+            );
+            echo("<br>Eliminado MediaWiki:Editnotice-0-$page");
+            sleep(10);
+        }
+    }
+
+    /**
+     * Create new pages with the provided page titles using the provided edit notice data.
+     * Note: The bot will sleep for 10 seconds after 
+     * each page creation in accordance with the local bot policy.
+     *
+     * @param array $pagesToCreate The list of page titles to create.
+     * @param string $editNoticeData The edit notice data to use when creating the pages.
+     */
+    private function createPages($pagesToCreate, $editNoticeData)
+    {
+        foreach ($pagesToCreate as $page) {
+            $this->edit(
+                "{{:$editNoticeData}}",
+                NULL,
+                TRUE,
+                "bot: Criando editnotice",
+                "MediaWiki:Editnotice-0-$page"
+            );
+            echo("<br>Criado MediaWiki:Editnotice-0-$page");
+            sleep(10);
+        }
+    }
+
+    /**
+     * Runs the Edit Notice Bot for a given category and Edit Notice data.
+     *
+     * @param string $category The name of the category to process.
+     * @param array $editNoticeData An array containing the data required for the Edit Notice.
+     *        It should have the following keys:
+     *        - "afinidade": The name of the category containing articles with strong ties to the country where the language variation is spoken.
+     *        - "escrito": The name of the category containing articles written in the language variation.
+     *        - "afluir": The name of the page that will be linked in the main Edit Notice.
+     */
+    public function run($category, $editNoticeData)
+    {
+        $needs = $this->getPagesThatShouldExist($editNoticeData);
+        $exists = $this->getPagesThatAlreadyExist($editNoticeData);
+
+        $toCreate = array_diff($needs, $exists);
+        $toDelete = array_diff($exists, $needs);
+
+        $this->deletePages($toDelete);
+        $this->createPages($toCreate, $editNoticeData["afluir"]);
+    }
 }
 
-//Lista de variantes
+/**
+ * Runs the EditNotices bot for multiple Wikipedia language versions.
+ *
+ * @param array $data An associative array where each key represents an ISO code for a language version, and the value is another associative array with the following keys:
+ *  - escrito: The name of the category containing articles written in the language variation.
+ *  - afinidade: The name of the category containing articles with strong ties to the country or region where the language variation is spoken.
+ *  - afluir: The name of the edit notice that should be associated with the articles.
+ */
 $data = [
-	"br" => [
-		"escrito" 		=> "Categoria:!Artigos escritos em português brasileiro",
-		"afinidade"		=> "Categoria:!Artigos que mantêm fortes afinidades com o Brasil",
-		"afluir"		=> "MediaWiki:Editnotice-0-Brasil"
-	],
-	"pt" => [
-		"escrito" 		=> "Categoria:!Artigos escritos em português europeu",
-		"afinidade"		=> "Categoria:!Artigos com fortes afinidades a Portugal",
-		"afluir"		=> "MediaWiki:Editnotice-0-Portugal"
-	],
-	"mz" => [
-		"escrito" 		=> "Categoria:!Artigos escritos em português moçambicano",
-		"afinidade"		=> "Categoria:!Artigos com fortes afinidades a Moçambique",
-		"afluir"		=> "MediaWiki:Editnotice-0-Moçambique"
-	],
-	"ao" => [
-		"escrito" 		=> "Categoria:!Artigos escritos em português angolano",
-		"afinidade"		=> "Categoria:!Artigos com fortes afinidades a Angola",
-		"afluir"		=> "MediaWiki:Editnotice-0-Angola"
-	]
+    "br" => [
+        "escrito"       => "Categoria:!Artigos escritos em português brasileiro",
+        "afinidade"     => "Categoria:!Artigos que mantêm fortes afinidades com o Brasil",
+        "afluir"        => "MediaWiki:Editnotice-0-Brasil"
+    ],
+    "pt" => [
+        "escrito"       => "Categoria:!Artigos escritos em português europeu",
+        "afinidade"     => "Categoria:!Artigos com fortes afinidades a Portugal",
+        "afluir"        => "MediaWiki:Editnotice-0-Portugal"
+    ],
+    "mz" => [
+        "escrito"       => "Categoria:!Artigos escritos em português moçambicano",
+        "afinidade"     => "Categoria:!Artigos com fortes afinidades a Moçambique",
+        "afluir"        => "MediaWiki:Editnotice-0-Moçambique"
+    ],
+    "ao" => [
+        "escrito"       => "Categoria:!Artigos escritos em português angolano",
+        "afinidade"     => "Categoria:!Artigos com fortes afinidades a Angola",
+        "afluir"        => "MediaWiki:Editnotice-0-Angola"
+    ]
 ];
-
+$api = new EditNotices('https://pt.wikipedia.org/w/api.php', $usernameEN, $passwordEN);
 foreach ($data as $iso => $variation) {
-
-	//Mensagem inicial
-	echo($variation["afluir"]." em processamento... ");
-
-	//Categoria de artigos escritos da variante
-	$escrito_params = [
-		"action" 		=> "query",
-		"format" 		=> "php",
-		"list" 			=> "categorymembers",
-		"cmtitle" 		=> $variation["escrito"],
-		"cmprop" 		=> "title",
-		"cmnamespace" 	=> "1",
-		"cmlimit" 		=> "max"
-	];
-	$escrito_api = unserialize(api_get($escrito_params));
-	foreach ($escrito_api["query"]["categorymembers"] as $page) $list_escrito[$iso][] = str_replace("/", "-", substr($page["title"], 11));
-	while (isset($escrito_api['continue'])) {
-		$escrito_params["cmcontinue"] = $escrito_api['continue']['cmcontinue'];
-		$escrito_api = unserialize(api_get($escrito_params));
-		foreach ($escrito_api["query"]["categorymembers"] as $page) $list_escrito[$iso][] = str_replace("/", "-", substr($page["title"], 11));
-	}
-
-	//Categoria de artigos da variante com forte afinidade
-	$afinidade_params = [
-		"action" 		=> "query",
-		"format" 		=> "php",
-		"list" 			=> "categorymembers",
-		"cmtitle" 		=> $variation["afinidade"],
-		"cmprop" 		=> "title",
-		"cmnamespace" 	=> "1",
-		"cmlimit" 		=> "max"
-	];
-	$afinidade_api = unserialize(api_get($afinidade_params));
-	foreach ($afinidade_api["query"]["categorymembers"] as $page) $list_afinidade[$iso][] = str_replace("/", "-", substr($page["title"], 11));
-	while (isset($afinidade_api['continue'])) {
-		$afinidade_params["cmcontinue"] = $afinidade_api['continue']['cmcontinue'];
-		$afinidade_api = unserialize(api_get($afinidade_params));
-		foreach ($afinidade_api["query"]["categorymembers"] as $page) $list_afinidade[$iso][] = str_replace("/", "-", substr($page["title"], 11));
-	}
-
-	//Une as duas categorias
-	$list_cats[$iso] = array_unique(array_merge($list_afinidade[$iso], $list_escrito[$iso]));
-
-	//Coleta lista de avisos já existentes via afluentes
-	$existentes_params = [
-		"action" 		=> "query",
-		"format" 		=> "php",
-		"list" 			=> "embeddedin",
-		"eititle" 		=> $variation["afluir"],
-		"einamespace" 	=> "8",
-		"eilimit" 		=> "max"
-	];
-	$existentes_api = unserialize(api_get($existentes_params));
-	foreach ($existentes_api["query"]["embeddedin"] as $page) $list_existentes[$iso][] = substr($page["title"], 23);
-	while (isset($existentes_api['continue'])) {
-		$existentes_params["eicontinue"] = $existentes_api['continue']['eicontinue'];
-		$existentes_api = unserialize(api_get($existentes_params));
-		foreach ($existentes_api["query"]["categorymembers"] as $page) $list_existentes[$iso][] = substr($page["title"], 23);
-	}
-
-	//Verifica se o aviso existente possui correspondencia na categoria
-	//Se não, elimina o aviso
-	foreach ($list_existentes[$iso] as $existente) {
-		if (!in_array($existente, $list_cats[$iso])) {
-			deleteAPI(
-				"MediaWiki:Editnotice-0-".$existente,
-				"G1 - [[WP:ER#ERg1|Eliminação técnica]] (bot: Eliminando editnotice desnecessário)",
-				$usernameEN
-			);
-			sleep(10);
-			echo("<br>Eliminar MediaWiki:Editnotice-0-".$existente);
-		}
-	}
-
-	//Verifica se o item da categoria possui aviso correspondente
-	//Se não, cria o aviso
-	foreach ($list_cats[$iso] as $item_cat) {
-		if ($variation["afluir"] == "MediaWiki:Editnotice-0-".$item_cat) continue;
-		if (!in_array($item_cat, $list_existentes[$iso])) {
-			editAPI(
-				"{{:".$variation["afluir"]."}}",
-				NULL,
-				TRUE,
-				"bot: Criando editnotice",
-				"MediaWiki:Editnotice-0-".$item_cat,
-				$usernameEN
-			);
-			sleep(10);
-			echo("<br>Criar MediaWiki:Editnotice-0-".$item_cat);
-		}
-	}
-
-	//Mensagem final
-	echo("OK!<br>");
+    $api->run($iso, $variation);
 }
