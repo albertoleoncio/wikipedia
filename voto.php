07cb2ea1812e610e02cbe4114ec113c24775cd19
albertoleoncio <40328018+albertoleoncio@users.noreply.github.com>
2023-04-07 13:18:59 -0300

    voto.php: Convertendo para OO

--- voto.php
+++ voto.php
@@ -1,120 +1,206 @@
 <?php
-
-//Define UTC como timezone
+require_once './bin/globals.php';
+require_once 'WikiAphpi/main.php';
 date_default_timezone_set('UTC');
 
-//Verifica se alguma usuário foi informado
-if ($_GET["user"]) {
+/**
+ * Class Suffrage
+ * This class verify a user's right to vote on a given time.
+ */
+class Suffrage extends WikiAphpiUnlogged
+{
 
-	// Condicionais para verificação de data/hora informada no formulário
-	// ╔══════╦══════╦════════════════════════════════════════════════╗
-	// ║ DATA ║ HORA ║ TRATAMENTO                                     ║
-	// ╠══════╬══════╬════════════════════════════════════════════════╣
-	// ║  S   ║  S   ║ Utiliza data/hora informada                    ║
-	// ╠══════╬══════╬════════════════════════════════════════════════╣
-	// ║  S   ║  N   ║ Define como o último segundo da data informada ║
-	// ╠══════╬══════╬════════════════════════════════════════════════╣
-	// ║      ║      ║ Verifica se horário informado é inferior ao    ║
-	// ║  N   ║  S   ║ horário UTC atual. Se sim, considera o dia     ║
-	// ║      ║      ║ atual, se não, considera o dia anterior.       ║
-	// ╠══════╬══════╬════════════════════════════════════════════════╣
-	// ║  N   ║  N   ║ Utiliza data/hora UTC atual                    ║
-	// ╚══════╩══════╩════════════════════════════════════════════════╝
-	//
-	if ((!isset($_GET["date"]) OR $_GET["date"] == "") AND (!isset($_GET["time"]) OR $_GET["time"] == "")) {
-		$date = date("o-m-d");
-		$time = date("H:i:s");
-	} elseif (isset($_GET["date"]) AND (!isset($_GET["time"]) OR $_GET["time"] == "")) {
-		$date = date("o-m-d", strtotime($_GET["date"]));
-		$time = "23:59:59";
-	} elseif ((!isset($_GET["date"]) OR $_GET["date"] == "") AND isset($_GET["time"])) {
-		$time = date("H:i:s", strtotime($_GET["time"]));
-		if ($time < date("H:i:s")) {
-			$date = date("o-m-d");
-		} else {
-			$date = date("o-m-d", strtotime($_GET["date"]." -1 day"));
-		}
-	} elseif (isset($_GET["date"]) AND isset($_GET["time"])) {
-		$date = date("o-m-d", strtotime($_GET["date"]));
-		$time = date("H:i:s", strtotime($_GET["time"]));
-	} else {
-		$date = date("o-m-d");
-		$time = date("H:i:s");
-	}
+    /**
+     * Processes the date and time provided by the user and returns a timestamp string in ISO 8601 format.
+     *
+     * This function accepts two optional parameters, $getDate and $getTime, which represent the date and time provided
+     * by the user, respectively. If both $getDate and $getTime are provided, this function returns a timestamp string
+     * representing that date and time. If only $getDate is provided, the function returns a timestamp string representing
+     * the last second of that day. If only $getTime is provided, the function returns a timestamp string representing the
+     * current date and the time provided, adjusted for the UTC timezone and if the time provided is after the current UTC 
+     * time, the function will return a timestamp string representing the previous day with the time provided. If neither 
+     * $getDate nor $getTime are provided, the function returns a timestamp string representing the current UTC date and time.
+     *
+     * @param string|null $getDate The date provided by the user, in YYYY-MM-DD format.
+     * @param string|null $getTime The time provided by the user, in HH:MM:SS format.
+     * @return string A timestamp string in ISO 8601 format, e.g. "2023-04-07T19:12:34Z".
+     */
+    private function processTimestamp($getDate, $getTime)
+    {
+        if (!empty($getDate) && !empty($getTime)) {
+            $date = date("o-m-d", strtotime($getDate));
+            $time = date("H:i:s", strtotime($getTime));
+        } elseif (!empty($getDate) && empty($getTime)) {
+            $date = date("o-m-d", strtotime($getDate));
+            $time = "23:59:59";
+        } elseif (empty($getDate) && !empty($getTime)) {
+            $time = date("H:i:s", strtotime($getTime));
+            if ($time < date("H:i:s")) {
+                $date = date("o-m-d");
+            } else {
+                $date = date("o-m-d", strtotime("$getDate -1 day"));
+            }
+        } else {
+            $date = date("o-m-d");
+            $time = date("H:i:s");
+        }
 
-	//Captura nome de usuário
-	$user = trim($_GET["user"]);
+        return "{$date}T{$time}Z";
+    }
 
-	//Coleta últimas 301 edições do usuário no domínio principal
-	$userquery = json_decode(file_get_contents("https://pt.wikipedia.org/w/api.php?action=query&uclimit=301&format=json&list=usercontribs&ucuser=".urlencode($user)."&ucstart=".urlencode($date)."T".urlencode($time)."Z&ucnamespace=0"), true)['query']['usercontribs'];
+    /**
+     * Counts the number of edits on the main domain made by a given user starting from a given timestamp.
+     *
+     * @param string $user The username of the user whose edits will be counted.
+     * @param string $timestamp The timestamp from which to start counting edits, in ISO 8601 format.
+     * @return int|false The number of edits on the main domain made by the user, or false if there was an error.
+     */
+    private function countUserEdits($user, $timestamp)
+    {
+        $params = [
+            'action'        => 'query',
+            'format'        => 'php',
+            'list'          => 'usercontribs',
+            'uclimit'       => '301',
+            'ucnamespace'   => '0',
+            'ucuser'        => $user,
+            'ucstart'       => $timestamp
+        ];
+        return count($this->see($params)['query']['usercontribs']) ?? false;
+    }
 
-	//Coleta timestamp da primeira edição
-	$timestamp = strtotime(json_decode(file_get_contents("https://pt.wikipedia.org/w/api.php?action=query&format=json&list=usercontribs&uclimit=1&ucuser=".urlencode($user)."&ucdir=newer&ucprop=timestamp"), true)['query']['usercontribs']['0']['timestamp']);
-	if ($timestamp != FALSE AND $timestamp < strtotime($date."T".$time."Z -90 days")) $idade = TRUE;
+    /**
+     * Retrieves the timestamp of a given user's first edit on Wikipedia.
+     *
+     * @param string $user The username of the user whose first edit timestamp will be retrieved.
+     * @return string|false The timestamp of the user's first edit, in ISO 8601 format, or false if there was an error.
+     */
+    private function checkFirstEditTimestamp($user)
+    {
+        $params = [
+            'action'    => 'query',
+            'format'    => 'json',
+            'list'      => 'usercontribs',
+            'uclimit'   => '1',
+            'ucuser'    => $user,
+            'ucdir'     => 'newer',
+            'ucprop'    => 'timestamp'
+        ];
+        return $this->see($params)['query']['usercontribs']['0']['timestamp'] ?? false;
+    }
 
-	//Define cor do resultado
-	if (count($userquery) >= "300" AND isset($idade)) {$color = "green";} else {$color = "red";}
+    /**
+     * Verifies if a user is eligible to vote based on their edit history and a given date and time.
+     * @param int $getUser The ID of the user to verify.
+     * @param string|null $getDate (Optional) The date in Y-m-d format to check the user's edit history, or null to use the current date.
+     * @param string|null $getTime (Optional) The time in H:i:s format to check the user's edit history, or null to use the current time.
+     * @return array An array with three elements:
+     *  - The number of edits made by the user on or before the specified timestamp.
+     *  - A string representing the user's eligibility status. Possible values are:
+     *    - 'DORMANT' if the user has never made an edit, including the main domain.
+     *    - 'IMMATURE' if the user's first edit was less than 90 days ago.
+     *    - 'INACTIVE' if the user have not made at least 300 edits.
+     *    - 'VALID' if the user has made at least 300 edits in the last 365 days and user's first edit was more than 90 days ago.
+     *  - A string representing the timestamp used for the verification, in ISO 8601 format adjusted for the UTC timezone.
+     */
+    public function verifySuffrage($getUser, $getDate, $getTime)
+    {
+        $timestamp = $this->processTimestamp($getDate, $getTime);
+        $count = $this->countUserEdits($getUser, $timestamp);
+        $first = $this->checkFirstEditTimestamp($getUser);
 
-	//Define resultado
-	if (!isset($idade)) {
-		$count = "Primeira edição recente<br>(Não possui direito ao voto)";
-	} elseif (count($userquery) == "301") {
-		$count = "Total de edições: 300+<br>(Possui direito ao voto)";
-	} elseif (count($userquery) == "300") {
-		$count = "Total de edições: 300<br>(Possui direito ao voto)";
-	} else {
-		$count = "Total de edições: ".count($userquery)."<br>(Não possui direito ao voto)";
-	}
+        if (!$first || !$count) {
+            return [0, 'DORMANT', $timestamp];
+        }
+
+        if ($first > strtotime("$timestamp -90 days")) {
+            return [0, 'IMMATURE', $timestamp];
+        }
+
+        if ($count < 300) {
+            return [$count, 'INACTIVE', $timestamp];
+        }
+
+        return [$count, 'VALID', $timestamp];
+    }
+}
+
+/**
+ * This code block instantiates the necessary class and runs the `verifySuffrage()` method
+ * of the `Suffrage` class with the provided parameters.
+ *
+ * If the `user` parameter is not set, the code block returns `false`.
+ */
+$getUser = filter_input(INPUT_GET, 'user', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$getDate = filter_input(INPUT_GET, 'date', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$getTime = filter_input(INPUT_GET, 'time', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+if ($getUser) {
+    $api = new Suffrage('https://pt.wikipedia.org/w/api.php');
+    $echo = $api->verifySuffrage($getUser, $getDate, $getTime);
+} else {
+    $echo = false;
 }
 ?><!DOCTYPE html>
 <html lang="pt-BR">
-	<head>
-		<title>Direito a voto</title>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="./tpar/w3.css">
-	</head>
-	<body>
-		<div class="w3-container" id="menu">
-			<div class="w3-content" style="max-width:800px">
-				<h5 class="w3-center w3-padding-48"><span class="w3-tag w3-wide">DIREITO A VOTO</span></h5>
-				<div class="w3-row-padding w3-center w3-margin-top">
-					<div class="w3-half">
-						<form action="/voto.php" method="get">
-							<div class="w3-container w3-padding-48 w3-card">
-		      					<p class="w3-center w3-wide">USUÁRIO</p>
-		      					<p class="w3-text-grey"><input class="w3-input w3-padding-16 w3-border" <?php if (isset($user) OR $user != "") echo "value='".$user."'"; ?> type="text" name="user"></p><br>
-		      					<div class="w3-half">
-			      					<p class="w3-center w3-wide">DATA</p>
-			      					<p class="w3-text-grey"><input class="w3-input w3-padding-16 w3-border" <?php if (isset($date) OR $date != "") echo "value='".$date."'"; ?> type="date" name="date"></p><br>
-			      				</div>
-			      				<div class="w3-half">
-			      					<p class="w3-center w3-wide">HORA</p>
-			      					<p class="w3-text-grey"><input class="w3-input w3-padding-16 w3-border" <?php if (isset($time) OR $time != "") echo "value='".$time."'"; ?> type="time" step="1" max="23:59:59" name="time"></p><br>
-		      					</div>
-		      					<small>Por favor insira a data e hora do começo da votação como Tempo Universal Coordenado (UTC).</small>
-			      				<button class="w3-button w3-block w3-black" type="submit">Verificar</button>
-		      				</div>
-		      			</form>
-		      		</div>
-		      		<div class="w3-half">
-		      			<div class="w3-container w3-padding-48 w3-card">
-		      				<?php
-		      				if ($_GET["user"]) {
-	  							if (isset($idade)){
-	  								$percent = floor( (count($userquery) * 100) / 300 );
-	  								echo "<div class='w3-light-grey'><div class='w3-container w3-".$color." w3-center' style='width:".$percent."%'>".$percent."%</div></div>";
-								}
-								echo "<p>".$count."</p>
-								<p>Usuário: ".$_GET["user"]."<br>Em ".$date." ".$time." UTC</p>";
-							} else {
-								echo "Preencha o formulário ao lado";
-							}?></div>
-		      		</div>
-		      	</div>
-      		</div>
-      	</div>
-		<hr>
-		<a href="https://wikitech.wikimedia.org/wiki/Portal:Toolforge"><img src="https://tools-static.wmflabs.org/toolforge/banners/Powered-by-Toolforge-button.png" alt="Powered by Wikimedia Toolforge"></a>
-	</body>
+    <head>
+        <title>Direito a voto</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./tpar/w3.css">
+    </head>
+    <body>
+        <div class="w3-container" id="menu">
+            <div class="w3-content" style="max-width:800px">
+                <h5 class="w3-center w3-padding-48"><span class="w3-tag w3-wide">DIREITO A VOTO</span></h5>
+                <div class="w3-row-padding w3-center w3-margin-top">
+                    <div class="w3-half">
+                        <form action="/voto.php" method="get">
+                            <div class="w3-container w3-padding-48 w3-card">
+                                <p class="w3-center w3-wide">USUÁRIO</p>
+                                <p class="w3-text-grey"><input class="w3-input w3-padding-16 w3-border" value="<?=$getUser??''?>" type="text" name="user"></p><br>
+                                <div class="w3-half">
+                                    <p class="w3-center w3-wide">DATA</p>
+                                    <p class="w3-text-grey"><input class="w3-input w3-padding-16 w3-border" value="<?=$getDate??''?>" type="date" name="date"></p><br>
+                                </div>
+                                <div class="w3-half">
+                                    <p class="w3-center w3-wide">HORA</p>
+                                    <p class="w3-text-grey"><input class="w3-input w3-padding-16 w3-border" value="<?=$getTime??''?>" type="time" step="1" max="23:59:59" name="time"></p><br>
+                                </div>
+                                <small>Por favor insira a data e hora do começo da votação como Tempo Universal Coordenado (UTC).</small>
+                                <button class="w3-button w3-block w3-black" type="submit">Verificar</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="w3-half">
+                        <div class="w3-container w3-padding-48 w3-card">
+                            <?php if (!$echo): ?>
+                                <p>Preencha o formulário ao lado</p>
+                            <?php else: ?>
+                                <div class='w3-light-grey'>
+                                    <div 
+                                    class='w3-container w3-<?=($echo['0']>=300)?'green':'red'?> w3-center' 
+                                    style='width:<?=floor($echo['0'] / 3)?>%'
+                                    ><?=floor($echo['0'] / 3)?>%</div>
+                                </div>
+                                <p>
+                                    <?php if($echo['1'] === 'DORMANT'): ?>
+                                        Sem edições<br>(Não possui direito ao voto)
+                                    <?php elseif($echo['1'] === 'IMMATURE'): ?>
+                                        Não atingiu idade mínima<br>(Não possui direito ao voto)
+                                    <?php elseif($echo['1'] === 'INACTIVE'): ?>
+                                        Total de edições: <?=$echo['0']?><br>(Não possui direito ao voto)
+                                    <?php else: ?>
+                                        Total de edições: >=300<br>(Possui direito ao voto)
+                                    <?php endif; ?>
+                                </p>
+                                <p>Usuário: <?=$getUser?><br>Em <?=$echo['2']?> UTC</p>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <a href="https://wikitech.wikimedia.org/wiki/Portal:Toolforge"><img src="https://tools-static.wmflabs.org/toolforge/banners/Powered-by-Toolforge-button.png" alt="Powered by Wikimedia Toolforge"></a>
+    </body>
 </html>
