767097422bde58abf65f0a4c8f2e6f1fc2283f96
albertoleoncio <albertoleoncio@hotmail.com>
2024-07-31 09:51:33 -0300

    Refactor telegramdaemon.php for user verification and restriction

--- telegramdaemon.php
+++ telegramdaemon.php
@@ -1,4 +1,4 @@
-<?php
+<pre><?php
 require_once './bin/globals.php';
 require_once 'WikiAphpi/main.php';
 
@@ -18,17 +18,21 @@ $update = json_decode($content, true)["result"];
 
 foreach ($update as $event) {
     $offset = $event["update_id"];
+    echo "Processing event... ${offset}\n";
     if (isset($event["chat_member"]["new_chat_member"])) {
 
         if ($event["chat_member"]["chat"]["id"] != "-1001169425230") {
+            echo "Ignoring chat...\n";
             continue;
         }
 
         if ($event["chat_member"]["new_chat_member"]["user"]["is_bot"]) {
+            echo "Ignoring bot...\n";
             continue;
         }
 
         if ($event["chat_member"]["new_chat_member"]["status"] != "member") {
+            echo "Ignoring non-member...\n";
             continue;
         }
 
@@ -45,11 +49,63 @@ foreach ($update as $event) {
             $ts_mycnf['password'],
             $ts_mycnf['user']."__telegram"
         );
-        $query = "INSERT IGNORE INTO `verifications` (`t_id`, `t_username`) VALUES ('$new_user_id', '$new_user')";
-        mysqli_query($con, $query);
-        mysqli_close($con);
-        
+
+        $query = "SELECT * FROM `verifications` WHERE `t_id` = '$new_user_id'";
+        $result = mysqli_query($con, $query);
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $w_id = $row['w_id'];
+
+            if ($w_id != null) {
+                echo "User already verified...\n";
+                continue;
+            }
+        } else {
+            $query = "INSERT IGNORE INTO `verifications` (`t_id`, `t_username`) VALUES ('$new_user_id', '$new_user')";
+            mysqli_query($con, $query);
+            mysqli_close($con);
+            echo "User added to database...\n";
+        }
+
+        # Get user status on Telegram
+        $params = [
+            'chat_id' => $event["chat_member"]["chat"]["id"],
+            'user_id' => $new_user_id
+        ];
+        $content = file_get_contents("https://api.telegram.org/bot${TelegramVerifyToken}/getChatMember?" . http_build_query($params));
+        $status = json_decode($content, true)["result"]["status"];
+
+        if ($status == "member") {
+            $params = [
+                'chat_id' => $event["chat_member"]["chat"]["id"],
+                'user_id' => $new_user_id,
+                "permissions" => [
+                    "can_send_messages" => false,
+                    "can_send_audios" => false,
+                    "can_send_documents" => false,
+                    "can_send_photos" => false,
+                    "can_send_videos" => false,
+                    "can_send_video_notes" => false,
+                    "can_send_voice_notes" => false,
+                    "can_send_polls" => false,
+                    "can_send_other_messages" => false,
+                    "can_add_web_page_previews" => false,
+                    "can_change_info" => false,
+                    "can_invite_users" => false,
+                    "can_pin_messages" => false,
+                    "can_manage_topics" => false,
+                ]
+            ];
+            $content = file_get_contents("https://api.telegram.org/bot${TelegramVerifyToken}/restrictChatMember?" . http_build_query($params));
+            $content = json_decode($content, true);
+            if ($content["ok"]) {
+                echo "Restricted ${new_user_id}...\n";
+            } else {
+                echo "Failed to restrict ${new_user_id}...\n";
+            }
+        }
     }
+    echo "Event processed...\n\n\n";
 }
 echo "Offset: $offset\n";
 file_put_contents("telegram_offset.inc", $offset);
