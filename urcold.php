b4d49ce814f55b9b1e8b94dfaa7458b11312d2f8
albertoleoncio <albertoleoncio@hotmail.com>
2026-06-24 18:45:03 -0300

    Refactor Urcold class to utilize WikiAphpi for API interactions and improve file modification tracking

--- urcold.php
+++ urcold.php
@@ -1,64 +1,119 @@
-<pre><?php
+<?php
 require_once './bin/globals.php';
+require_once 'WikiAphpi/main.php';
 date_default_timezone_set('UTC');
 
-//Login
-require_once './bin/api.php';
-loginAPI($username, $password);
+class Urcold extends WikiAphpiLogged
+{
+	/**
+	 * Gets files listed in the ESR category for yesterday.
+	 *
+	 * @return array
+	 */
+	private function getTargetFiles()
+	{
+		$params = [
+			'action' => 'query',
+			'list' => 'categorymembers',
+			'cmtitle' => 'Categoria:!Ficheiros para eliminação semirrápida/dia ' . date('j', strtotime('-1 day')),
+		];
 
-/*
-	get_subcategories.php
+		$response = $this->see($params);
+		return $response['query']['categorymembers'] ?? [];
+	}
 
-	MediaWiki API Demos
-	Demo of `Categorymembers` module : Get ten subcategories of a category
+	/**
+	 * Retrieves upload timestamp for a file page.
+	 *
+	 * @param string $title
+	 * @return int|false
+	 */
+	private function getFileTimestamp($title)
+	{
+		$params = [
+			'action' => 'query',
+			'prop' => 'imageinfo',
+			'titles' => $title,
+			'iiprop' => 'timestamp',
+		];
 
-	MIT License
-*/
+		$response = $this->see($params);
+		$pages = $response['query']['pages'] ?? [];
+		$pageData = reset($pages);
+		$timestamp = $pageData['imageinfo'][0]['timestamp'] ?? false;
 
-$endPoint = "https://pt.wikipedia.org/w/api.php";
-$params_cat = [
-	"action" => "query",
-	"list" => "categorymembers",
-	"cmtitle" => "Categoria:!Ficheiros para eliminação semirrápida/dia ".date("j", strtotime("-1 day")),
-	"format" => "json"
-];
+		if ($timestamp === false) {
+			return false;
+		}
 
-$ch_cat = curl_init( $endPoint . "?" . http_build_query( $params_cat ) );
-curl_setopt( $ch_cat, CURLOPT_RETURNTRANSFER, true );
-$output_cat = curl_exec( $ch_cat );
-curl_close( $ch_cat );
-$result_cat = json_decode( $output_cat, true );
+		return strtotime($timestamp);
+	}
 
-//Loop de análise de cada arquivo encontrado
-foreach( $result_cat["query"]["categorymembers"] as $file ) {
+	/**
+	 * Updates a file page to include "modificado = sim".
+	 *
+	 * @param string $page
+	 * @return int
+	 */
+	private function markAsModified($page)
+	{
+		$wikiCode = $this->get($page);
+		$newCode = str_replace('nformação', "nformação\n| modificado = sim", $wikiCode);
 
-	$params_file = [
-		"action" => "query",
-		"prop" => "imageinfo",
-		"titles" => $file["title"],
-		"format" => "json"
-	];
-	
-	$ch_file = curl_init( $endPoint . "?" . http_build_query( $params_file ) );
-	curl_setopt( $ch_file, CURLOPT_RETURNTRANSFER, true );
-	$output_file = curl_exec( $ch_file );
-	curl_close( $ch_file );
-	$result_file = json_decode( $output_file, true );
+		return $this->edit(
+			$newCode,
+			null,
+			true,
+			'bot: Inserindo parâmetro "modificado" para evitar eliminação ([[Predefinição Discussão:Informação#Pergunta_técnica_II|detalhes]])',
+			$page
+		);
+	}
 
-	//Recupera data de envio do arquivo
-	$timestamp = strtotime(pos($result_file['query']['pages'])['imageinfo']['0']['timestamp']);
+	/**
+	 * Runs the workflow and returns an execution report.
+	 *
+	 * @return array
+	 */
+	public function run()
+	{
+		$report = [
+			'checked_files' => 0,
+			'eligible_files' => 0,
+			'edited_files' => [],
+			'skipped_without_timestamp' => [],
+		];
 
-	//Condicional para verificar se arquivo foi enviado antes de 28/05/2011 (dif:25470547)
-	if ($timestamp < 1306540800) {
+		$files = $this->getTargetFiles();
 
-		//Recupera conteúdo do arquivo
-		$page = $file["title"];
-		$wikiCode = getAPI($page);
+		foreach ($files as $file) {
+			$page = $file['title'];
+			$report['checked_files']++;
 
-		//Insere: modificado = sim
-		$wikiCode = str_replace("nformação","nformação\n| modificado = sim", $wikiCode);
+			$timestamp = $this->getFileTimestamp($page);
+			if ($timestamp === false) {
+				$report['skipped_without_timestamp'][] = $page;
+				continue;
+			}
 
-		//Gravar código
-		editAPI($wikiCode, NULL, true, "bot: Inserindo parâmetro \"modificado\" para evitar eliminação ([[Predefinição Discussão:Informação#Pergunta_técnica_II|detalhes]])", $page, $username);
+			// Files uploaded before 2011-05-28 (diff 25470547).
+			if ($timestamp < 1306540800) {
+				$report['eligible_files']++;
+				$revisionId = $this->markAsModified($page);
+				$report['edited_files'][] = [
+					'page' => $page,
+					'revision_id' => $revisionId,
+				];
+			}
+		}
+
+		$report['message'] = (count($report['edited_files']) === 0)
+			? 'Nenhum ficheiro elegível precisou de edição nesta execução.'
+			: 'Execução concluída com atualizações em ficheiros elegíveis.';
+
+		return $report;
 	}
 }
+
+// Run script
+$api = new Urcold('https://pt.wikipedia.org/w/api.php', $username, $password);
+print_r($api->run());
