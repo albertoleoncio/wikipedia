da8c0c9c28b7f9ebaf3a857a09df8f650503309e
albertoleoncio <albertoleoncio@hotmail.com>
2026-06-24 18:41:27 -0300

    Refactor BlockReverts class to utilize WikiAphpi for API calls and improve rollback processing

--- blockreverts.php
+++ blockreverts.php
@@ -1,97 +1,201 @@
-<pre><?php
+<?php
 require_once './bin/globals.php';
+require_once 'WikiAphpi/main.php';
 
-//Login
-require_once './bin/api.php';
-loginAPI($usernameBQ, $passwordBQ);
+class BlockReverts extends WikiAphpiLogged
+{
+	/** @var string */
+	private $reportPage = 'Usuário(a):BloqBot/Reversões';
 
-//Levanta lista de autorrevisores
-$autoreviers_API = file_get_contents("https://pt.wikipedia.org/w/api.php?action=query&format=php&list=allusers&augroup=autoreviewer%7Cbureaucrat%7Celiminator%7Csysop&aulimit=500");
-$autoreviers_API = unserialize($autoreviers_API)["query"]["allusers"];
+	/** @var string */
+	private $donePage = 'Usuário(a):BloqBot/revd';
 
-//Insere ID de autorrevisores em uma array
-$autoreviers_IDs = array();
-foreach ($autoreviers_API as $user) $autoreviers[] = $user["name"];
+	/**
+	 * Retrieves all users in autoreviewer-related groups.
+	 *
+	 * @return array
+	 */
+	private function getAutoreviewers()
+	{
+		$users = [];
+		$params = [
+			'action'  => 'query',
+			'list'    => 'allusers',
+			'augroup' => 'autoreviewer|bureaucrat|eliminator|sysop',
+			'aulimit' => '500',
+		];
 
-//Continua lista de autorrevisores
-while (isset($autoreviers_API['continue'])) {
-	$continue_autoreviers_API = $autoreviers_API['continue']['aufrom'];
-	$autoreviers_API = file_get_contents("https://pt.wikipedia.org/w/api.php?action=query&format=php&list=allusers&augroup=autoreviewer%7Cbureaucrat%7Celiminator%7Csysop&aulimit=500&aufrom=".urlencode($continue_autoreviers_API));
-	foreach ($autoreviers_API as $user) $autoreviers[] = $user["name"];
-}
+		do {
+			$response = $this->see($params);
+			foreach ($response['query']['allusers'] ?? [] as $user) {
+				$users[] = $user['name'];
+			}
 
-//Levanta lista de reversões ocorridos nos últimos 30 minutos
-$rollbacks_API = file_get_contents("https://pt.wikipedia.org/w/api.php?action=query&format=php&list=recentchanges&rctag=mw-rollback&rcprop=title%7Cuser%7Cids%7Ccomment%7Ctags&rclimit=500&rcend=".urlencode(gmdate('Y-m-d\TH:i:s.000\Z', strtotime("-30 minutes"))));
-$rollbacks_API = unserialize($rollbacks_API)["query"]["recentchanges"];
+			if (isset($response['continue']['aufrom'])) {
+				$params['aufrom'] = $response['continue']['aufrom'];
+			} else {
+				unset($params['aufrom']);
+			}
+		} while (isset($response['continue']['aufrom']));
 
-//Processa cada registro de reversão
-$notify = array();
-foreach ($rollbacks_API as $rollback) {
+		return array_unique($users);
+	}
 
-	//Recupera nome do alvo
-	preg_match_all('/(?:Foi \[\[WP:REV\|revertida\]\] a edição|Foram \[\[WP:REV\|revertidas\]\] as edições) de \[\[Special:Contrib(?:s|uições|utions)\/\K[^\]\|]*/', $rollback["comment"], $rollbacked);
+	/**
+	 * Retrieves rollback-tagged recent changes from the last 30 minutes.
+	 *
+	 * @return array
+	 */
+	private function getRecentRollbacks()
+	{
+		$params = [
+			'action'  => 'query',
+			'list'    => 'recentchanges',
+			'rctag'   => 'mw-rollback',
+			'rcprop'  => 'title|user|ids|comment|tags',
+			'rclimit' => '500',
+			'rcend'   => gmdate('Y-m-d\\TH:i:s.000\\Z', strtotime('-30 minutes')),
+		];
 
-	//Pula caso a reversão tenha sido manual
-	if (!isset($rollbacked["0"]["0"])) continue;
+		$response = $this->see($params);
+		return $response['query']['recentchanges'] ?? [];
+	}
 
-	//Pula caso tenha sido autorreversão
-	if ($rollbacked["0"]["0"] == $rollback["user"]) continue;
+	/**
+	 * Extracts rollback cases that should be reported.
+	 *
+	 * @param array $rollbacks
+	 * @param array $autoreviewers
+	 * @return array
+	 */
+	private function getCasesToNotify(array $rollbacks, array $autoreviewers)
+	{
+		$notify = [];
 
-	//Pula caso o revertido não seja autorrevisor
-	if (!in_array($rollbacked["0"]["0"], $autoreviers)) continue;
+		foreach ($rollbacks as $rollback) {
+			preg_match_all(
+				'/(?:Foi \[\[WP:REV\|revertida\]\] a edição|Foram \[\[WP:REV\|revertidas\]\] as edições) de \[\[Special:Contrib(?:s|uições|utions)\/\K[^\]\|]*/',
+				$rollback['comment'] ?? '',
+				$rollbacked
+			);
 
-	//Guarda dados da reversão
-	$notify[] = [
-		"id"		=> $rollback["revid"],
-		"user"		=> $rollback["user"],
-		"title"		=> $rollback["title"],
-		"target"	=> $rollbacked["0"]["0"]
-	];
-}
+			if (!isset($rollbacked[0][0])) {
+				continue;
+			}
 
-//Define páginas
-$page = "Usuário(a):BloqBot/Reversões";
-$done_page = "Usuário(a):BloqBot/revd";
+			if (($rollbacked[0][0] ?? '') === ($rollback['user'] ?? '')) {
+				continue;
+			}
 
-//Recupera lista de reversões já lançadas
-$done_list = explode("\n", file_get_contents("https://pt.wikipedia.org/w/index.php?title=Usu%C3%A1rio(a):BloqBot/revd&action=raw"));
+			if (!in_array($rollbacked[0][0], $autoreviewers, true)) {
+				continue;
+			}
 
-//Loop para inserir incidentes na página
-foreach ($notify as $case) {
+			$notify[] = [
+				'id'     => $rollback['revid'],
+				'user'   => $rollback['user'],
+				'title'  => $rollback['title'],
+				'target' => $rollbacked[0][0],
+			];
+		}
 
-	//Pula caso reversão já tenha sido lançada
-	if (in_array($case["id"], $done_list)) continue;
+		return $notify;
+	}
 
-	//Recupera codigo-fonte da página
-	$html = getAPI($page);
+	/**
+	 * Sends a single notification message to Telegram.
+	 *
+	 * @param array $case
+	 * @return string|false
+	 */
+	private function sendTelegram(array $case)
+	{
+		global $TelegramToken;
 
-	//Insere pedido no código
-	$html = $html."\n#{{dif|".$case["id"]."}}: Usuário '".$case["target"]."' revertido por '".$case["user"]."' na página [[:".$case["title"]."]].";
-
-	//Gravar código
-	editAPI($html, NULL, FALSE, "bot: Inserindo reversão não-usual", $page, $usernameBQ);
-
-	//Recupera codigo-fonte da lista de reversões já lançadas
-	$done_html = getAPI($done_page);
-
-	//Gravar código na lista de reversões já lançadas
-	editAPI($done_html."\n".$case["id"], NULL, FALSE, "bot: Lançando ID de reversão", $done_page, $usernameBQ);
-
-	//Envia conteúdo para grupo no Telegram
-	$telegram_context = array(
-		'http' => array(
-			'method' => 'POST',
-			'header' => "Content-Type:application/x-www-form-urlencoded\r\n",
-			'content' => http_build_query(
-				array(
-					'chat_id' => -1001169425230,
+		$telegramContext = [
+			'http' => [
+				'method'  => 'POST',
+				'header'  => "Content-Type:application/x-www-form-urlencoded\r\n",
+				'content' => http_build_query([
+					'chat_id'    => -1001169425230,
 					'parse_mode' => 'MarkdownV2',
-					'text' => "[\[Δ".$case["id"]."\]](https://pt.wikipedia.org/wiki/Special:diff/".$case["id"]."): [".$case["target"]."](https://pt.wikipedia.org/wiki/User:".$case["target"].") revertido por [".$case["user"]."](https://pt.wikipedia.org/wiki/User:".$case["user"].") em [".$case["title"]."](https://pt.wikipedia.org/wiki/".$case["title"].")\."
-				)
-			)
-		)
-	);
-	$telegram_stream = stream_context_create($telegram_context);
-	$telegram = file_get_contents('https://api.telegram.org/bot'.$TelegramToken.'/sendMessage', false, $telegram_stream);
-	print_r($telegram);
+					'text'       => '[\\[Δ' . $case['id'] . '\\]](https://pt.wikipedia.org/wiki/Special:diff/' . $case['id'] . '): [' . $case['target'] . '](https://pt.wikipedia.org/wiki/User:' . $case['target'] . ') revertido por [' . $case['user'] . '](https://pt.wikipedia.org/wiki/User:' . $case['user'] . ') em [' . $case['title'] . '](https://pt.wikipedia.org/wiki/' . $case['title'] . ')\\.',
+				]),
+			],
+		];
+
+		$telegramStream = stream_context_create($telegramContext);
+		return file_get_contents('https://api.telegram.org/bot' . $TelegramToken . '/sendMessage', false, $telegramStream);
+	}
+
+	/**
+	 * Runs the workflow to detect unusual rollbacks and report them.
+	 *
+	 * @return array
+	 */
+	public function run()
+	{
+		$startedAt = date('c');
+		$autoreviewers = $this->getAutoreviewers();
+		$rollbacks = $this->getRecentRollbacks();
+		$notify = $this->getCasesToNotify($rollbacks, $autoreviewers);
+
+		$doneList = explode("\n", $this->get($this->donePage));
+		$report = [
+			'started_at' => $startedAt,
+			'report_page' => $this->reportPage,
+			'done_page' => $this->donePage,
+			'autoreviewers_count' => count($autoreviewers),
+			'rollbacks_in_window' => count($rollbacks),
+			'candidates_found' => count($notify),
+			'already_reported_skipped' => 0,
+			'new_cases_processed' => 0,
+			'processed_case_ids' => [],
+			'telegram_ok' => 0,
+			'telegram_failed' => 0,
+			'telegram_responses' => [],
+		];
+
+		foreach ($notify as $case) {
+			if (in_array($case['id'], $doneList, true)) {
+				$report['already_reported_skipped']++;
+				continue;
+			}
+
+			$reportContent = $this->get($this->reportPage);
+			$reportContent .= "\n#{{dif|" . $case['id'] . "}}: Usuário '" . $case['target'] . "' revertido por '" . $case['user'] . "' na página [[:" . $case['title'] . ']].';
+
+			$this->edit($reportContent, null, false, 'bot: Inserindo reversão não-usual', $this->reportPage);
+
+			$doneContent = $this->get($this->donePage);
+			$this->edit($doneContent . "\n" . $case['id'], null, false, 'bot: Lançando ID de reversão', $this->donePage);
+			$report['new_cases_processed']++;
+			$report['processed_case_ids'][] = $case['id'];
+
+			$telegramResult = $this->sendTelegram($case);
+			if ($telegramResult === false) {
+				$report['telegram_failed']++;
+			} else {
+				$report['telegram_ok']++;
+			}
+			$report['telegram_responses'][] = [
+				'case_id' => $case['id'],
+				'response' => $telegramResult,
+			];
+		}
+
+		$report['finished_at'] = date('c');
+
+		if ($report['new_cases_processed'] === 0) {
+			$report['message'] = 'Nenhum caso novo para registrar nesta execução.';
+		} else {
+			$report['message'] = 'Execução concluída com novos casos registrados.';
+		}
+
+		return $report;
+	}
 }
+
+// Run script
+$api = new BlockReverts('https://pt.wikipedia.org/w/api.php', $usernameBQ, $passwordBQ);
+print_r($api->run());
